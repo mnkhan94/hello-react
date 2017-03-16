722b8296601412b644f121418c581bcfd4e95ba1
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-16 00:01:32 -0700

    Starting App

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./entry.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+};
